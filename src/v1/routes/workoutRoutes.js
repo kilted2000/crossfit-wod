ca90591cabb3a65ec3ae4c22e45f5d92f8cbc54e
Richard <richardlwhittington@gmail.com>
2024-04-16 16:15:26 -0500

    add cacheing

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const apicache = require("apicache");
 const workoutController = require("../../controllers/workoutController");
 // *** ADD ***
 const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
+const cache = apicache.middleware;
 
-router.get("/", workoutController.getAllWorkouts);
+router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
 
 router.get("/:workoutId", workoutController.getOneWorkout);
 
