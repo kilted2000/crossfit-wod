a208b0e4cebfd1af561800d266e33d983013eff4
Richard <richardlwhittington@gmail.com>
2024-04-16 22:03:16 -0500

    finish api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const bodyParser = require("body-parser"); 
+const bodyParser = require("body-parser");
+const apicache = require("apicache");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 const app = express(); 
+const cache = apicache.middleware;
 const PORT = process.env.PORT || 3000; 
 
 // For testing purposes 
 app.use(bodyParser.json());
+app.use(cache("2 minutes"));
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 
@@ -17,4 +21,5 @@ app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => { 
     console.log(`API is listening on port ${PORT}`); 
+    V1SwaggerDocs(app, PORT);
 });
